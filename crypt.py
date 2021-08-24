070cf59167e6d131a9b36ac73a7a34589882bbe5
pygudev <gubaev.andreb@gmail.com>
2021-08-25 00:01:29 +0500

    Доработал модуль шифрования

--- crypt.py
+++ crypt.py
@@ -1,15 +1,20 @@
 from cryptography.fernet import Fernet
+import base64
 
 
 class Crypt:
     def __init__(self, key: str):
-        self._key: bytes = key.encode()
+        self._key: bytes = self._convert_key(key)
         self._f = Fernet(self._key)
 
-    def encrypt(self, value: str) -> bytes:
-        encrypted_value = self._f.encrypt(value.encode())
-        return encrypted_value
+    @staticmethod
+    def _convert_key(key: str) -> bytes:
+        return base64.urlsafe_b64encode(key.encode()[:32])
 
-    def decrypt(self, value: bytes) -> str:
-        decrypted_value = self._f.decrypt(value)
+    def encrypt(self, value: str) -> str:
+        encrypted_value = self._f.encrypt(value.encode())
+        return encrypted_value.decode('utf-8')
+
+    def decrypt(self, value: str) -> str:
+        decrypted_value = self._f.decrypt(value.encode())
         return decrypted_value.decode('utf-8')
